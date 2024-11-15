11994eaff12e6d2fdb8e1dbbbd050f49e90911f8
datx24 <xuandat475@gmail.com>
2024-11-15 23:02:59 +0700

    tạo controller đăng nhập

---
+++ MovieWeb/MovieWeb/Controllers/Account_64130299Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MovieWeb.Models;
+
+namespace MovieWeb.Controllers
+{
+    public class Account_64130299Controller : Controller
+    {
+        // Danh sách người dùng quản trị viên (có thể thay đổi sau này)
+        private static List<User_64130299> _adminUsers = new List<User_64130299>
+        {
+            new User_64130299 { Username = "admin", Password = "password" }
+        };
+
+        // Trang đăng nhập
+        public ActionResult Login_64130299()
+        {
+            return View();
+        }
+
+        // Xử lý đăng nhập
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = AuthenticateUser(username, password);
+            if (user != null)
+            {
+                // Lưu thông tin đăng nhập vào session hoặc cookie để xác thực người dùng
+                Session["Username"] = user.Username;
+                return RedirectToAction("Index", "Admin"); // Redirect đến trang quản trị
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng.");
+            }
+            return View();
+        }
+
+        // Phương thức xác thực người dùng
+        private User_64130299 AuthenticateUser(string username, string password)
+        {
+            // Tìm người dùng trong danh sách quản trị viên
+            return _adminUsers.FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
+
+        // Đăng xuất
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
