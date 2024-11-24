f2cc625e9dee55fecde65d2865b4d5e1d9c97693
datx24 <xuandat475@gmail.com>
2024-11-24 22:59:28 +0700

    tạo controller cho giao diện thanh toán

---
+++ MovieWeb/MovieWeb/Controllers/Subscription_64130299Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieWeb.Controllers
+{
+    public class Subscription_64130299Controller : Controller
+    {
+        // GET: Subscription_64130299
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
