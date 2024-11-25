e4c1c35670edfd2be882dbe9ba48d50e6095ff98
datx24 <xuandat475@gmail.com>
2024-11-25 14:08:14 +0700

    tạo url thanh toán và chuyển hướng người dùng đến cổng VNPay.

--- MovieWeb/MovieWeb/Controllers/Subscription_64130299Controller.cs
+++ MovieWeb/MovieWeb/Controllers/Subscription_64130299Controller.cs
@@ -78,5 +78,36 @@ namespace MovieWeb.Controllers
         {
             return View();
         }
+        public ActionResult VNPayPayment(string subscriptionId)
+        {
+            var subscription = db.MemberSubscription_64130299.FirstOrDefault(x => x.SubscriptionId == subscriptionId);
+
+            if (subscription == null)
+            {
+                return RedirectToAction("Failure_64130299");
+            }
+
+            string vnp_Url = System.Configuration.ConfigurationManager.AppSettings["vnp_Url"];
+            string vnp_ReturnUrl = System.Configuration.ConfigurationManager.AppSettings["vnp_ReturnUrl"];
+            string vnp_TmnCode = System.Configuration.ConfigurationManager.AppSettings["vnp_TmnCode"];
+            string vnp_HashSecret = System.Configuration.ConfigurationManager.AppSettings["vnp_HashSecret"];
+
+            VNPayLibrary vnpay = new VNPayLibrary();
+            vnpay.AddRequestData("vnp_Version", "2.1.0");
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+            vnpay.AddRequestData("vnp_Amount", (subscription.Amount * 100).ToString());
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_TxnRef", subscription.SubscriptionId);
+            vnpay.AddRequestData("vnp_OrderInfo", $"Thanh toán gói {subscription.PlanId} cho User {subscription.UserId}");
+            vnpay.AddRequestData("vnp_OrderType", "billpayment");
+            vnpay.AddRequestData("vnp_Locale", "vn");
+            vnpay.AddRequestData("vnp_ReturnUrl", vnp_ReturnUrl);
+            vnpay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss"));
+
+            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+            return Redirect(paymentUrl);
+        }
+
     }
 }
